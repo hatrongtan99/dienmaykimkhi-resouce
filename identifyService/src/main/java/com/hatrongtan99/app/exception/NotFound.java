27443b2f8d29b3d984653ddd8e659e6cb3f531ed
tanha <hatrongtan99@gmail.com>
2023-12-22 16:17:34 +0700

    delete identity

--- identifyService/src/main/java/com/hatrongtan99/app/exception/NotFound.java
+++
@@ -1,7 +0,0 @@
-package com.hatrongtan99.app.exception;
-
-public class NotFound extends RuntimeException{
-    public NotFound(String message) {
-        super(message);
-    }
-}
--- identifyService/src/main/java/com/hatrongtan99/app/repository/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.hatrongtan99.app.repository;
-
-import com.hatrongtan99.app.entity.AuthorityEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<AuthorityEntity, Long> {
-}
